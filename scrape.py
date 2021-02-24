60e07abb5fc0abc342d9e9e920b435021a5f77b3
Alex Brady <alex.brady@blueface.com>
2021-02-24 16:07:59 +0000

    initial commit

---
+++ scrape.py
@@ -0,0 +1,15 @@
+import requests
+import json
+import numpy as np
+import pandas as pd
+import datetime
+
+# Make a get request to get the latest player data from the FPL API
+response = requests.get("https://fantasy.premierleague.com/api/bootstrap-static/")
+
+data = json.loads(response.text)
+
+team_names = {team['id']: team['name'] for team in data['teams']}
+
+players = {player['id']: f'{player["first_name"]} {player["second_name"]}' for player in data['elements']}
+
